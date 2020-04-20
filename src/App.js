78643387c3db28e07e5cb48b9e982e9e2fca9c92
Logan Bauer <lbp1108@hotmail.com>
2020-04-20 13:55:12 -0300

    #ENC - Implementação de likes e ajustes layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   SafeAreaView,
@@ -9,47 +9,78 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
+import api from "./services/api";
 
 export default function App() {
+  const [ repositories , setRepositories ] = useState([]);
+
+  useEffect(() =>  {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post(`/repositories/${id}/like`);
+
+    const likedRepository = response.data;
+
+    const repositoriesUpdated = repositories.map(repository => {
+      if(repository.id === id){
+        return likedRepository;
+      } else {
+        return repository;
+      }
+    });
+
+    setRepositories(repositoriesUpdated);
   }
 
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <StatusBar barStyle="light-content" backgroundColor="#3DC2BF"/>
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <Text style={styles.appTitle}>Desafio React Native GoStack</Text>
+          <FlatList
+            data={repositories}
+            keyExtractor={repository => repository.id}
+            renderItem={({ item: repository }) => ( 
+                <View style={styles.repositoryContainer}>
+                <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+                <View style={styles.techsContainer}>
+                  <FlatList
+                    style={styles.flatlistTechs}
+                    data={repository.techs}
+                    keyExtractor={item => item}
+                    renderItem={({ item: tech }) => (
+                      <Text style={styles.tech}>
+                        {tech}
+                      </Text>
+                    )}
+                  />
+                </View>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+                <View style={styles.likesContainer}>
+                  <Text
+                    style={styles.likeText}
+                    testID={`repository-likes-${repository.id}`}
+                  >
+                    {repository.likes} curtidas
+                  </Text>
+                </View>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+                <TouchableOpacity
+                  style={styles.button}
+                  activeOpacity={0.8}
+                  onPress={() => handleLikeRepository(repository.id)}
+                  testID={`like-button-${repository.id}`}
+                >
+                  <Text style={styles.buttonText}>Curtir</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+        />
       </SafeAreaView>
     </>
   );
@@ -58,17 +89,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#7159c1",
+    backgroundColor: "#3DC2BF",
+  },
+  appTitle:{
+    alignSelf: 'center',
+    color: '#FFF',
+    fontSize: 20,
+    marginTop: 5,
   },
   repositoryContainer: {
+    alignItems: 'center',
+    marginTop: 10,
     marginBottom: 15,
     marginHorizontal: 15,
-    backgroundColor: "#fff",
+    backgroundColor: "#FFF",
     padding: 20,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.48,
+    shadowRadius: 11.95,   
+    elevation: 18,
   },
   repository: {
     fontSize: 32,
     fontWeight: "bold",
+    color: '#3DC2BF',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.48,
+    shadowRadius: 11.95,   
+    elevation: 18,
+    textAlign: 'center'
   },
   techsContainer: {
     flexDirection: "row",
@@ -78,10 +136,18 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "bold",
     marginRight: 10,
-    backgroundColor: "#04d361",
+    backgroundColor: "#3DC2BF",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    color: "#fff",
+    color: "#FFF",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.48,
+    shadowRadius: 11.95,   
+    elevation: 18,
   },
   likesContainer: {
     marginTop: 15,
@@ -89,19 +155,33 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   likeText: {
-    fontSize: 14,
+    fontSize: 15,
     fontWeight: "bold",
     marginRight: 10,
   },
   button: {
     marginTop: 10,
+    alignSelf: 'stretch'
   },
   buttonText: {
-    fontSize: 14,
+    fontSize: 16,
     fontWeight: "bold",
-    marginRight: 10,
-    color: "#fff",
-    backgroundColor: "#7159c1",
+    color: "#FFF",
+    backgroundColor: "#3DC2BF",
     padding: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.48,
+    shadowRadius: 11.95,   
+    elevation: 18,
+    textAlign: 'center'
+  },
+  flatlistTechs: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    textAlign: 'center'
   },
 });
